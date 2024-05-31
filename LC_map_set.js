782462bd7627f9135e9477ca768b8c37e0872311
Timofey <sukonkin.timofei@gmail.com>
2024-05-31 18:22:41 +0300

    task map/set DONE!!!

--- LC_map_set.js
+++ LC_map_set.js
@@ -1,6 +1,9 @@
-//1 Создайте функцию uniqueValues, которая принимает массив значений и возвращает новый массив, содержащий только уникальные значения из входного массива.
+//1 Создайте функцию uniqueValues, которая принимает массив 
+//значений и возвращает новый массив, содержащий только 
+//уникальные значения из входного массива.
 
-function uniqueValues(values) {
+{
+  function uniqueValues(values) {
     let uniqueValuesArray = new Set(values);
   
     return Array.from(uniqueValuesArray);
@@ -10,9 +13,15 @@ function uniqueValues(values) {
   const values = [1, 2, 2, 3, 4, 4, 5];
   const uniqueValuesArray = uniqueValues(values);
   // console.log(uniqueValuesArray); // [1, 2, 3, 4, 5]
-  
-  //2 Реализуйте класс Storage, который будет использовать Map для хранения данных. Класс должен предоставлять методы setItem для добавления данных, getItem для извлечения данных по ключу и removeItem для удаления данных по ключу.
-  
+}
+
+
+//2 Реализуйте класс Storage, который будет использовать 
+//Map для хранения данных. Класс должен предоставлять методы 
+//setItem для добавления данных, getItem для извлечения данных 
+//по ключу и removeItem для удаления данных по ключу.
+
+{
   class Storage {
     constructor(value, key) {
       this.map = new Map();
@@ -40,12 +49,19 @@ function uniqueValues(values) {
   // console.log(storage.getItem('name')); // John
   // storage.removeItem('age');
   // console.log(storage.getItem('age')); // undefined
-  
-  //3 Реализуйте функцию memoize, которая принимает функцию func и возвращает новую функцию, которая кэширует результаты вызовов func для уникальных аргументов. Используйте Map для хранения результатов и WeakMap для хранения ссылок на объекты, чтобы избежать утечек памяти.
-  
+}
+
+//3 Реализуйте функцию memoize, которая принимает функцию 
+//func и возвращает новую функцию, которая кэширует 
+//результаты вызовов func для уникальных аргументов. 
+//Используйте Map для хранения результатов и WeakMap 
+//для хранения ссылок на объекты, чтобы избежать утечек памяти.
+
+
+{
   function memoize(func) {
     let map = new Map();
-    return function(obj) {
+    return function (obj) {
       let result = func(obj);
       if (!map.has(obj)) {
         map.set(obj, result);
@@ -71,48 +87,53 @@ function uniqueValues(values) {
   // console.log(memoizedOperation(obj1)); // Expensive operation, 20
   // console.log(memoizedOperation(obj1)); // 20 (из кэша)
   // console.log(memoizedOperation(obj2)); // Expensive operation, 40
-  
-  //4 Реализуйте функцию trackUniqueObjectsWithWeakSet, 
-  //которая принимает массив объектов и возвращает новый 
-  //массив, содержащий только уникальные объекты. 
-  //Используйте WeakSet для отслеживания уже встреченных 
-  // объектов. 
-  //Дополнительно, реализуйте функцию removeObject, 
-  //которая позволяет удалять объекты из WeakSet по ссылке.
+}
 
+
+
+
+//4 Реализуйте функцию trackUniqueObjectsWithWeakSet, 
+//которая принимает массив объектов и возвращает новый 
+//массив, содержащий только уникальные объекты. 
+//Используйте WeakSet для отслеживания уже встреченных 
+// объектов. 
+//Дополнительно, реализуйте функцию removeObject, 
+//которая позволяет удалять объекты из WeakSet по ссылке.
+
+{
   const objects = [{ id: 1 }, { id: 2 }, { id: 1 }];
-  const uniqueObjectsSet = new WeakSet()
-  
-  function trackUniqueObjectsWithWeakSet(objects) {
-    let arr = [];
-    
-   objects.forEach((el, i) => {
-    let item = JSON.stringify(el);
-    uniqueObjectsSet.add(JSON.parse(item));
-    console.log(uniqueObjectsSet.has(JSON.parse(item)));
-   });
+  const uniqueObjectsSet = new WeakSet();
 
-  //  objects.forEach((el, i) => {
 
-  //  })
 
-  //  for(let el of map.values()) {
-  //   uniqueObjectsSet.add(el);
-  //   arr.push(el);
-  //  }
-    return arr;
-  
+function trackUniqueObjectsWithWeakSet(objects) {
+  let arr = [];
+  let workSet = new Set();
+
+  objects.forEach(el => {
+    workSet.add(JSON.stringify(el));
+  });
+
+  for(let el of workSet) {
+    el = JSON.parse(el);
+    arr.push(el);
+    uniqueObjectsSet.add(el);
   }
-  
-  function removeObject(obj) {
-    //...
-  }
-  
-  // Пример использования
-  
-  const uniqueObjects = trackUniqueObjectsWithWeakSet(objects);
-  console.log(uniqueObjects); // [{ id: 1 }, { id: 2 }]
-  
-  // Удаление объекта из WeakSet
-  // removeObject(uniqueObjects[0]);
-  // console.log(uniqueObjectsSet.has(uniqueObjects[0])); // false
+  return arr;
+}
+
+function removeObject(obj) {
+  uniqueObjectsSet.delete(obj)
+}
+
+// Пример использования
+
+const uniqueObjects = trackUniqueObjectsWithWeakSet(objects);
+console.log(uniqueObjects); // [{ id: 1 }, { id: 2 }]
+
+
+// Удаление объекта из WeakSet
+removeObject(uniqueObjects[0]);
+console.dir(uniqueObjectsSet); // value: {id: 2}
+console.log(uniqueObjectsSet.has(uniqueObjects[0])); // false
+}
